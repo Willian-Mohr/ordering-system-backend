c0b7419b9aaa36883cebeda15fe3879ef032603d
Willian-Mohr <willianbmohr@gmail.com>
2021-05-03 23:21:30 -0300

    Projeto criado

---
+++ src/main/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.orderingsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.orderingsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
