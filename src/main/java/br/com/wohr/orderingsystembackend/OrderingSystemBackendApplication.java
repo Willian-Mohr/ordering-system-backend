9f188f91fd600b068e3da3d9e4fc28723cf3ebfc
Willian-Mohr <willianbmohr@gmail.com>
2021-05-04 15:28:41 -0300

    Criando operações de Instanciação

--- src/main/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplication.java
+++ src/main/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplication.java
@@ -1,13 +1,29 @@
 package br.com.wohr.orderingsystembackend;
 
+import br.com.wohr.orderingsystembackend.domain.Categoria;
+import br.com.wohr.orderingsystembackend.repositories.CateroriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class OrderingSystemBackendApplication {
+public class OrderingSystemBackendApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderingSystemBackendApplication.class, args);
-	}
+    @Autowired
+    CateroriaRepository cateroriaRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(OrderingSystemBackendApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+
+        cateroriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
