91b8052bbc6e908053e5bcb858b0f4b1e087b748
Willian-Mohr <willianbmohr@gmail.com>
2021-05-06 13:40:27 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplication.java
+++ src/main/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplication.java
@@ -1,7 +1,9 @@
 package br.com.wohr.orderingsystembackend;
 
 import br.com.wohr.orderingsystembackend.domain.Categoria;
+import br.com.wohr.orderingsystembackend.domain.Produto;
 import br.com.wohr.orderingsystembackend.repositories.CateroriaRepository;
+import br.com.wohr.orderingsystembackend.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class OrderingSystemBackendApplication implements CommandLineRunner {
     @Autowired
     CateroriaRepository cateroriaRepository;
 
+    @Autowired
+    ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(OrderingSystemBackendApplication.class, args);
     }
@@ -24,6 +29,18 @@ public class OrderingSystemBackendApplication implements CommandLineRunner {
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
 
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().add(p2);
+
+        p1.getCategorias().add(cat1);
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().add(cat1);
+
         cateroriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
---
+++ src/main/java/br/com/wohr/orderingsystembackend/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package br.com.wohr.orderingsystembackend.repositories;
+
+import br.com.wohr.orderingsystembackend.domain.Categoria;
+import br.com.wohr.orderingsystembackend.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
