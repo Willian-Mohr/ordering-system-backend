c151d4f5b523b3fc24e23eb53685c488bd793f3c
Willian-Mohr <willianbmohr@gmail.com>
2021-05-19 00:10:13 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/br/com/wohr/orderingsystembackend/repositories/ClienteRepository.java
+++ src/main/java/br/com/wohr/orderingsystembackend/repositories/ClienteRepository.java
@@ -3,7 +3,11 @@ package br.com.wohr.orderingsystembackend.repositories;
 import br.com.wohr.orderingsystembackend.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
