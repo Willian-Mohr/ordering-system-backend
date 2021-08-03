ee3d60a2bb69316ce030b276f9270870d1ab11bd
Willian-Mohr <willianbmohr@gmail.com>
2021-08-02 22:47:36 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplication.java
+++ src/main/java/br/com/wohr/orderingsystembackend/OrderingSystemBackendApplication.java
@@ -1,5 +1,7 @@
 package br.com.wohr.orderingsystembackend;
 
+import br.com.wohr.orderingsystembackend.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderingSystemBackendApplication implements CommandLineRunner {
 
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(OrderingSystemBackendApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        s3Service.uploadFile("W:\\Udemy\\Spring Boot\\Projetos\\PDF\\Secao-7\\uploadAmazonS3.jpg");
     }
 }
