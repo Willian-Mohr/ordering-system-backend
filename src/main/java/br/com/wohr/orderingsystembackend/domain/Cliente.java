fce7e37264bde47c13cc11b6ef6060af25877624
Willian-Mohr <willianbmohr@gmail.com>
2021-08-16 21:35:41 -0300

    Correção no CPF ou CNPJ do cliente

--- src/main/java/br/com/wohr/orderingsystembackend/domain/Cliente.java
+++ src/main/java/br/com/wohr/orderingsystembackend/domain/Cliente.java
@@ -19,6 +19,7 @@ public class Cliente implements Serializable {
 
     @Column(unique = true)
     private String email;
+    @Column(unique = true)
     private String cpfOuCnpj;
     private Integer tipo;
 
