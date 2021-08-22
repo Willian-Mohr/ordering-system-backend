ec9ffc4cb35107409cfd762491e40cf165116df7
Willian Mohr <willian.mohr@benner.com.br>
2021-08-22 15:55:29 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/br/com/wohr/orderingsystembackend/config/SecurityConfig.java
+++ src/main/java/br/com/wohr/orderingsystembackend/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC_MATCHERS_GET = {
             "/produtos/**",
             "/categorias/**",
-            "/clientes/changePassword/**"
+            "/clientes/changePassword/**",
+            "/estados/**"
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {
